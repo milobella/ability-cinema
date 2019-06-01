194af95865b8a6e4c4de7fce4a843eb7a90ebdc7
Célian Garcia <celian.garcia1@gmail.com>
2019-06-01 10:22:05 +0000

    Industrialize cinema ability

--- README.md
+++ README.md
@@ -1,11 +1,60 @@
-# Code skeleton of a Milobella ability written in go
+# Cinema ability
+Milobella ability to know about series & movies.
 
-# Build
+## Features
+- See what is coming out or soon in your favorite theater;
+- Manage your favorite theater;
+
+## Prerequisites
+
+- Having access to [gitlab.milobella.com](https://gitlab.milobella.com/milobella)
+- Having ``golang`` installed [instructions](https://golang.org/doc/install)
+- Having ``go dep`` installed [instructions](https://golang.github.io/dep/docs/installation.html)
+
+## Build
+
 ```bash
+$ dep ensure
 $ go build -o bin/ability cmd/ability/main.go
 ```
 
-# Run
+## Run
+
 ```bash
-$ ./bin/ability
+$ bin/ability
 ```
+
+## Requests example
+
+#### Get the movies for this evening in favorite theater.
+
+```bash
+$ curl -i -X POST http://localhost:4444/resolve -d '{"nlu":{"BestIntent": "LAST_SHOWTIME"}}'
+HTTP/1.1 200 OK
+Date: Sat, 01 Jun 2019 09:17:16 GMT
+Content-Type: text/plain; charset=utf-8
+Transfer-Encoding: chunked
+
+{
+	"nlg": {
+		"sentence": "Here are the movies in {{theater}} this evening, in the {{location}}'s theater",
+		"params": [{
+			"name": "theater",
+			"value": "La Strada",
+			"type": "string"
+		}, {
+			"name": "location",
+			"value": "Mouans-Sartoux",
+			"type": "string"
+		}]
+	},
+	"visu": [{
+		"title": "\"Toy Story 4\"",
+		"display": "\"Séances du dimanche 23 juin 2019 : 11:00 (film à 11:10)\""
+	}, ... ]
+}
+```
+
+## CHANGELOGS
+- [Application changelog](./CHANGELOG.md)
+- [Helm chart changelog](./helm/ability-cinema/CHANGELOG.md)
--- cmd/ability/main.go
+++ cmd/ability/main.go
@@ -1,20 +1,72 @@
 package main
 
 import (
-	"gitlab.milobella.com/milobella/abilities/ability-sdk-go/pkg/ability"
-	"gitlab.milobella.com/milobella/abilities/cinema-ability/pkg/tools/allocine"
+	"gitlab.milobella.com/milobella/ability-sdk-go/pkg/ability"
+	"gitlab.milobella.com/milobella/cinema-ability/pkg/tools/allocine"
+	"os"
+
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/viper"
 )
 
-var allocineClient = allocine.NewClient("http://0.0.0.0", 8000)
+var allocineClient *allocine.Client
+var allocineHost string
+var allocinePort int
+
+var additionalConfigPath string
+
+//TODO: try to put some common stuff into a separate repository
+func init() {
+	viper.SetConfigName("config")
+	viper.SetConfigType("yaml")
+
+	additionalConfigPath = os.Getenv("ADDITIONAL_CONFIG_PATH")
+	if len(additionalConfigPath) != 0 {
+		viper.AddConfigPath(additionalConfigPath)
+	}
+
+	viper.AddConfigPath(".")
+	viper.SetDefault("log-level", "info")
+
+	logrus.SetFormatter(&logrus.TextFormatter{})
+
+	// Output to stdout instead of the default stderr
+	// Can be any io.Writer, see below for File example
+	logrus.SetOutput(os.Stdout)
+
+	// TODO: read it in the config when move to viper
+	logrus.SetLevel(logrus.DebugLevel)
+
+	err := viper.ReadInConfig() // Find and read the config file
+	if err != nil { // Handle errors reading the config file
+		logrus.Errorf("Fatal error config file: %s \n", err)
+	}
+
+	if level, err := logrus.ParseLevel(viper.GetString("server.log-level")); err == nil {
+		logrus.SetLevel(level)
+	} else {
+		logrus.Warn("Failed to parse the log level. Keeping the logrus default level.")
+	}
+
+	logrus.Debugf("Configuration -> %+v", viper.AllSettings())
+
+	allocineHost = viper.GetString("tools.allocine.host")
+	allocinePort = viper.GetInt("tools.allocine.port")
+}
 
 // fun main()
 func main() {
-	server := ability.NewServer(10200)
-	server.RegisterIntent("LAST_SHOWTIME", lastShowTimeHandler)
+
+	// Initialize client for allocine tool
+	allocineClient = allocine.NewClient(allocineHost, allocinePort)
+
+	// Initialize server
+	server := ability.NewServer("Cinema Ability", viper.GetInt("server.port"))
+	server.RegisterIntentRule("LAST_SHOWTIME", lastShowTimeHandler)
 	server.Serve()
 }
 
-func lastShowTimeHandler(req ability.Request, resp *ability.Response) {
+func lastShowTimeHandler(_ *ability.Request, resp *ability.Response) {
 	result, err := allocineClient.GetLastShowTime("Mouans-Sartoux")
 	if err != nil {
 		resp.Nlg.Sentence = "Error"
@@ -25,9 +77,9 @@ func lastShowTimeHandler(req ability.Request, resp *ability.Response) {
 	location, _ := result.Path("feed.theaterShowtimes.place.theater.city").Children()
 
 	resp.Nlg.Sentence = "Here are the movies in {{theater}} this evening, in the {{location}}'s theater"
-	resp.Nlg.Params = map[string]string{
-		"theater":  theater[0].Data().(string),
-		"location": location[0].Data().(string),
+	resp.Nlg.Params = []ability.NLGParam{
+		{ Name: "theater", Value: theater[0].Data().(string), Type: "string" },
+		{ Name: "location", Value: location[0].Data().(string), Type: "string" },
 	}
 
 	showTimesBug, _ := result.Path("feed.theaterShowtimes.movieShowtimes").Children()
